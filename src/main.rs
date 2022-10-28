71ecc3620dccd52b42734e4ecaaffee0cab2c135
Eirik Bjørøen <bjoroen.eirik@gmail.com>
2022-10-28 22:39:28 +0200

    started on the tokenlexer thing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{env, str, fs::File, io::prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+      if args.len() > 2 {
+          println!("{}", args.len());
+          println!("Usage: Yafika [script]");
+          panic!("Panic and stuff")
+      }
+
+    if args.len() == 2 {
+        run_file(&args[1]);
+    }
+}
+
+pub fn run_file(file: &String) {
+    let mut f = File::open(file).expect("No File Found!");
+    let mut buffer = Vec::new();
+
+    f.read_to_end(&mut buffer).expect("something went wrong");
+
+    let string_from_vec = str::from_utf8(&buffer);
+    run(string_from_vec.unwrap())
+}
+
+
+pub fn run(source: &str) {
+    println!("{}", source)
 }
